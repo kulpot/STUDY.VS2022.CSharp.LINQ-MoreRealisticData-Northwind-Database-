cf3a7d360b44ae2e1b1e10ad45692307cee7cb5e
Kulpot <kulpotwahu@gmail.com>
2023-07-06 00:36:26 +0800

    Add project files.

---
+++ LINQ MoreRealisticData(Northwind Database)/Entities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_MoreRealisticData_Northwind_Database_
+{
+    class Customer
+    {
+        public string CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CompanyName { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ LINQ MoreRealisticData(Northwind Database)/Program.cs
@@ -0,0 +1,21 @@
+﻿using LINQ_MoreRealisticData_Northwind_Database_;
+using System;
+using System.Linq;
+
+//ref link:https://www.youtube.com/watch?v=_TasFjNcmUg&list=PL90AF0EFFEF782D27&index=16
+// LINQ(Language Integrated Query) - EntityFramework - Northwind DB
+
+static class MainClass
+{
+    static void Main()
+    {
+        IEnumerable<Customer> customers = DB.Customers;
+        foreach(Customer c in customers.Take(5))
+        {
+            Console.WriteLine(c.ContactName);
+            Console.WriteLine(c.CompanyName);
+            Console.WriteLine(c.Country);
+            Console.WriteLine();
+        }
+    }
+}
